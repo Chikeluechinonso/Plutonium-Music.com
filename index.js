79555650abde7b125e964ddf79eb455ebeed908d
Chinonso Chikelue <104786122+Chinonso-chikelue756@users.noreply.github.com>
2022-07-04 21:24:44 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+
+// Selecting the html class and  
+// convert it to an object
+  
+const sharebtn = 
+    document.querySelector('.sharebtn')
+  
+// Creating a bool variable for changing
+// the image of share button 
+var bool = 0
+  
+// Adding an event listener
+sharebtn.addEventListener('click', () => {
+  
+    // As we clicked the mouse over
+    // the share button the bool value.
+    //  get flipped and then working of
+    // if-else loop get starts
+    bool = !bool
+      
+    if (bool == 0) {
+        sharebtn.innerHTML =
+            '<i class="far fa-share-square"></i>'
+    } else {
+        sharebtn.innerHTML =
+            '<i class="fas fa-times"></i>'
+    }
+})
---
+++ main.js
@@ -0,0 +1,83 @@
+let btn = document.querySelectorAll('#track #play_pause');
+let song = document.querySelectorAll('#music');
+
+let song_index = 0;
+let song_status = 0;
+let index_no;
+
+btn.forEach((btn,index) => {
+  btn.addEventListener('click', function(){
+
+  	if (song_index == 1) {
+  		document.querySelector(".active_song").pause();
+  		document.querySelector(".active_song").classList.remove("active_song");
+  		document.querySelector(".active_btn").innerHTML = "<i class='bx bx-play'></i>";
+  		document.querySelector(".active_btn").classList.remove("active_btn");
+  	}else{
+        song_index = 1;
+  	}
+  	
+  	song[index].classList.add("active_song");
+    this.classList.add("active_btn");
+
+  	song[index].currentTime = 0;
+
+  	if (song_status == 0) {
+  		song[index].play();
+  		song_status = 1;
+  		setInterval(update_second, 1000);
+  		index_no = index;
+  		this.innerHTML = "<i class='bx bx-pause'></i>";
+  	}else{
+  		song[index].pause();
+  		this.innerHTML = "<i class='bx bx-play'></i>";
+  		song_status = 0;
+  	}
+
+  });
+});
+
+/*This function will be execute in Every Second*/
+ function update_second(){
+	if (song[index_no].ended) {
+      btn[index_no].innerHTML = "<i class='bx bx-play'></i>";
+      clearInterval(update_second);
+    }
+ }
+
+
+/*side menu*/
+function menu_action(){
+	let side_nav = document.querySelector('.side_menu');
+    side_nav.classList.toggle("side_nav_active");
+}
+
+/*main menu*/
+function open_close_menu(){
+	let nav_content = document.querySelector('.nav_content');
+    nav_content.classList.toggle("active_top_navbar");
+}
+
+
+
+// Function execute When the user scrolls the page
+window.onscroll = () =>{
+
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("Bar").style.width = scrolled + "%";
+}
+
+
+filterSelection("all")
+function filterSelection(c) {
+  var x, i ;
+  x =
+  document.getElementsByClassName("filterDiv");
+  if (c =="all") c = " " ;
+  // Add the "show" class 
+  ('display:block')
+}
+window.alert("Dear User,Our website is still under construction.We are working to make sure that our Comment section is responsive which is from our database and other minor problems.We promise to fix it soon");
+console.log('Dear User,Our website is still under construction.We are working to make sure that our Comment section is responsive which is from our database and other minor problems.We promise to fix it soon')
